220bbede5e59ee80a1848d709e565fe58856675b
CriSmile92 <crismile206@gmail.com>
2023-12-26 21:11:16 +0300

    refactiring even.js

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 <a href="https://codeclimate.com/github/CriSmile92/frontend-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/2cc40607b879a2f2ce5b/maintainability" /></a>
 
-https://asciinema.org/a/619057
+https://asciinema.org/a/629056
 
 https://asciinema.org/a/619054
 
--- src/games/even.js
+++ src/games/even.js
@@ -1,35 +1,16 @@
-import  readlineSync from 'readline-sync';
+import runGame from '../index.js';
+import getRandomNumber from '../utils.js';
 
-console.log('Welcome to the Brain Games!');
+const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
 
- const getName = () => readlineSync.question('May I have your name? ');
- 
- const userName = getName();
- 
- console.log(`Hello, ${userName}!`);
+const isEven = (number) => number % 2 === 0;
 
-  const getRandomInt = (max) => {
-    return Math.floor(Math.random() * max);
-  };
-  export const evenGame = () => {
-    console.log('Answer "yes" if the number is even, otherwise answer "no".');
-   
-    for (let i = 0; i < 3; i += 1) {
-      const number = getRandomInt(100);
-      console.log('Question: ', number);
-      const answerUser = readlineSync.question('Your answer: ');
-   
-      const checking1 = (number % 2 === 0) && (answerUser === 'yes');
-      const checking2 = (number % 2 !== 0) && (answerUser === 'no');
-   
-      if (checking1 === true || checking2 === true) {
-        console.log('Correct!');
-      } else {
-        const opposite = (answerUser === 'yes' ? 'no' : 'yes');
-        console.log(`"${answerUser}" is wrong answer ;(. Correct answer was '${opposite}'.\nLet's try again, ${userName}!`);
-    
-     return;
-      }
-    }
-    console.log(`Congratulations, ${userName}!`);
-    }
+const getGameData = () => {
+  const randomNumber = getRandomNumber(1, 100);
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+  return [randomNumber, correctAnswer];
+};
+
+const startGame = () => runGame(gameDescription, getGameData);
+
+export default startGame;
