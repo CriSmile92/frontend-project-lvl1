6a43a4e571a430c9124baee4718e8225920139ba
CriSmile92 <crismile206@gmail.com>
2023-12-26 22:05:13 +0300

    edits2

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
-const maxNumRand = 100;
-const minNumRand = 0;
-
-const getRandomNumber = (minNum = minNumRand, maxNum = maxNumRand) => Math.floor(Math.random() * maxNum) + minNum;
-
-export default getRandomNumber;
+const getRandomNumber = (min, max) => {
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    return result;
+  };
+  
+  export default getRandomNumber;
