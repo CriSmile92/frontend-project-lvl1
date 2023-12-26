1a064f6aba970a1e158efb74627debace375c930
CriSmile92 <crismile206@gmail.com>
2023-12-26 22:29:24 +0300

    new edits

--- src/utils.js
+++ src/utils.js
@@ -2,13 +2,7 @@ const maxNumRand = 100;
 const minNumRand = 0;
 
 const getRandomNumber = (minNum = minNumRand, maxNum = maxNumRand) => {
-      const result = Math.floor(Math.random() * maxNum) + minNum;
-      return result
+  const result = Math.floor(Math.random() * maxNum) + minNum;
+  return result;
 };
 export default getRandomNumber;
-/*const getRandomNumber = (min, max) => {
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
-    return result;
-  };
-  
-  export default getRandomNumber;*/
